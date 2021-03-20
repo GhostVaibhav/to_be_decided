4095fc97ba924a22b94206741615ee6562aba880
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2021-03-20 20:59:49 +0530

    Added quotes page

---
+++ Scripts/quote.js
@@ -0,0 +1,10 @@
+let requestURL = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const res = request.response;
+    document.getElementById("quot").innerHTML = res.quotes[0].text;
+    document.getElementById("auth").innerHTML = res.quotes[0].author;
+}
